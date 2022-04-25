ac5cc2d2803b0d274c4278ae9c8a1001aef7fb88
vladislav sorokin <sorokin.wlad@yandex.ru>
2022-04-25 15:42:29 +0600

    first attemp

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask('MyHerokuApp')
+
+# Read the mailgun secret key from environment variables
+mailgun_secret_key_value = os.environ.get('MAILGUN_SECRET_KEY', None)
+
+# This is needed for Heroku configuration, as in Heroku our
+# app will probably not run on port 5000, as Heroku will automatically
+# assign a port for our application
+port = int(os.environ.get("PORT", 5000))
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    # We will just display our mailgun secret key, nothing more.
+    return render_template("index.html", value=mailgun_secret_key_value)
 
 
-if __name__ == '__main__':
-    app.run(debug=False)
+app.run(host='0.0.0.0', port=port, debug=True)
